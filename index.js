c69e4b0771a0a882d6c3d5b4b5a9237aa579299e
Richie Casto <rcasto92@gmail.com>
2020-07-01 00:13:25 -0700

    split out cli code into cli.js, hook up cli.js in package.json

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const timeRanges = [
  * @param {string} packageName
  * @returns {Promise<PackageInfo>}
  */
-async function getNpmPackageInfo(packageName) {
+export async function getNpmPackageInfo(packageName) {
     let countLastDayDownloads = -1;
     let countLastWeekDownloads = -1;
     let countLastMonthDownloads = -1;
@@ -58,63 +58,4 @@ async function getNpmPackageInfo(packageName) {
         countLastYearDownloads,
         success: countLastDayDownloads >= 0
     };
-}
-
-const npmPackages = process.argv.slice(2);
-
-if (npmPackages.length <= 0) {
-    console.log(`Usage: npx ${commandName} <package1> <package2> ... <packageN> or ${commandName} <package1> <package2> ... <packageN>`);
-} else {
-    Promise.all(npmPackages
-        .map(packageName => getNpmPackageInfo(packageName)))
-        .then(packageResults => {
-            const successfulPackageResults = [];
-            const failedPackageResults = [];
-            packageResults
-                .forEach(packageResult => {
-                    if (packageResult.success &&
-                        typeof packageResult.countLastDayDownloads === 'number') {
-                        successfulPackageResults.push(packageResult);
-                    } else {
-                        failedPackageResults.push(packageResult);
-                    }
-                });
-            return [
-                successfulPackageResults,
-                failedPackageResults
-            ];
-        })
-        .then(splitPackageResults => {
-            const [
-                successfulPackageResults,
-                failedPackageResults
-            ] = splitPackageResults;
-
-            if (successfulPackageResults.length > 0) {
-                console.table(successfulPackageResults
-                    .reduce((prevObj, currPackageInfo) => {
-                        const {
-                            countLastDayDownloads,
-                            countLastWeekDownloads,
-                            countLastMonthDownloads,
-                            countLastYearDownloads
-                         } = currPackageInfo;
-                        return {
-                            ...prevObj,
-                            [currPackageInfo.packageName]: {
-                                countLastDayDownloads,
-                                countLastWeekDownloads,
-                                countLastMonthDownloads,
-                                countLastYearDownloads
-                            }
-                        };
-                    }, {}));
-            }
-
-            if (failedPackageResults.length > 0) {
-                const failedPackageNames = failedPackageResults
-                    .map(packageResult => packageResult.packageName);
-                console.log(`Failed to get download stats for the following packages: ${failedPackageNames.join(', ')}`);
-            }
-        });
 }
